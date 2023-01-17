bd736e60e92bfb7ad1674d8457d21307149e74c5
Yasin Aslan <mbpy@MBP-von-Yasin.fritz.box>
2023-01-17 10:20:27 +0100

    adding post component

--- client/front-react/src/App.js
+++ client/front-react/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { CreateUserPage } from './components/CreateUserPage/CreateUserPage';
 import { LoginPage } from './components/LoginPage/LoginPage';
+import { ResetPassword } from './components/ResetPassword/ResetPassword';
 import Nav from './components/Navigation/Nav.jsx';
 import Post from './components/Post/Post';
-import { ResetPassword } from './components/ResetPassword/ResetPassword';
 import SideBarLeft from './components/SideBarLeft/SideBarLeft';
 import SideBarRight from './components/SideBarRight/SideBarRight';
 import Trending from './components/Trending/Trending';
@@ -16,12 +16,12 @@ function App() {
       {/* <LoginPage /> */}
       <Nav />
       <div className='flex-app'>
-      <SideBarLeft />
-      <div className='mainContent'>
-      <Trending />
-      <Post />
-      </div>
-      <SideBarRight />
+        <SideBarLeft />
+        <div className='mainContent'>
+          <Trending />
+          <Post />
+        </div>
+          <SideBarRight />
       </div>
     </div>
   );
--- client/front-react/src/components/Post/Post.jsx
+++ client/front-react/src/components/Post/Post.jsx
@@ -1,22 +1,94 @@
 import React from 'react'
 import './post.css'
 import {FaArrowUp, FaArrowDown,FaCommentAlt,FaShare,FaBookmark} from 'react-icons/fa';
-const POST = 
+const POSTS = [  
   {
     id: 1,
-    title: "The title of the Post",
+    title: "Title one",
     username: "JohnDoe",
     avatar: "https://via.placeholder.com/50",
     likes: 300,
-  }
+  },
+  {
+    id: 2,
+    title: "Title two",
+    username: "JaneDoe",
+    avatar: "https://via.placeholder.com/50",
+    likes: 300,
+  },
+  {
+    id: 3,
+    title: "Title three",
+    username: "JonasDoe",
+    avatar: "https://via.placeholder.com/50",
+    likes: 300,
+  },
+  {
+    id: 4,
+    title: "Title four",
+    username: "JusDoe",
+    avatar: "https://via.placeholder.com/50",
+    likes: 300,
+  },
+
+]
 
 
 export default function Post() {
   return (
     <div className='post-container'>
-            <div className="post-content">
+                {POSTS.map((post)=> {
+                  return(
+                    <div className="post-content">
+                      <div className="post-header">
+                        <div className="userIcon">
+                        <img src={post.avatar} alt="userimg" />
+                        <p>@{post.username}</p>
+                      </div>
+                      <h3>{post.title}</h3>
+                    </div>
 
-              <div className="post-header">
+              <div className="post-main">
+                <div className="left-btn">
+                  <FaArrowUp className='arrows' />
+                  <p>{post.likes}</p>
+                  <FaArrowDown className='arrows' />
+                </div>
+                <div className="main-content"></div>
+              </div>
+
+                <div className="bottom-btn">
+                  <button className="comment-btn">
+                    <FaCommentAlt className='bottom-icons' />
+                    <p>255 Comments</p>
+                  </button>
+                  <button className="share-btn">
+                    <FaShare className='bottom-icons'  />
+                    <p>Share</p>  
+                  </button>
+                  <button className="save-btn">
+                    <FaBookmark className='bottom-icons'  />
+                    <p>Save</p>
+                  </button>
+                </div>
+            </div>
+                  )
+                })}
+    </div>
+  )
+}
+
+
+
+
+
+
+
+
+// Incase the map method breaks, hardcoded jsx here
+
+/*
+<div className="post-header">
                 <div className="userIcon">
                   <img src={POST.avatar} alt="userimg" />
                   <p>@{POST.username}</p>
@@ -47,7 +119,5 @@ export default function Post() {
                     <p>Save</p>
                   </button>
                 </div>
-            </div>
-    </div>
-  )
-}
+
+*/
